05471060e063dcc98952d2762b18a8a3f8e2c64e
James04123 <jamesbryancastillo04@gmail.com>
2025-09-14 20:49:06 +0700

    Add files via upload

---
+++ MyPortfolio.java
@@ -0,0 +1,125 @@
+public class MyPortfolio {
+        public static void main(String[] args) {
+
+                // about me
+                String name = "James Bryan Castillo";
+                String age = "18 Years Old";
+                String bday = "April 27, 2007";
+                String course = "Bachelor of Science in Computer Science (BSCS)";
+                String school = "University of Northern Philippines";
+                String info = "Email- jamesbryancastillo04@gmail.com";
+                String no = "Contact no.- 09672352244";
+                String add = "Gabor Norte, Sta.Cruz, Ilocos Sur";
+
+                // text colors
+                String reset = "\u001B[0m";
+                String purpleb = "\u001B[45m";
+                String cb = "\u001B[1;36m";
+                String green = "\u001B[32m";
+                String bgreen = "\u001B[1;32m";
+                String blue = "\u001B[34m";
+                String bblue = "\u001B[1;34m";
+
+                System.out.println(purpleb +
+                                ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+                                + reset
+                                + "\n");
+                System.out.println(blue + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++  " + reset + cb
+                                + "MY PORTFOLIO" + reset + blue
+                                + "  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + reset + "\n");
+                System.out.println(purpleb +
+                                "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+                                + reset
+                                + "\n\n");
+                System.out.println("\t\t\t\t\t\t\t" + bgreen + "<<< ABOUT ME >>>" + reset + "\n\n");
+
+                System.out.println(bgreen + "NAME:" + reset + "\t\t" + name + "\t\t\t\t\t\t" + course + "\n");
+                System.out.println(bgreen + "AGE:" + reset + "\t\t" + age + "\t\t\t\t\t\t\t" + school + "\n");
+                System.out.println(bgreen + "BIRTHDATE:" + reset + "\t" + bday + "\t\t\t\t\t\t\t" + no + "\n");
+                System.out.println(bgreen + "ADDRESS:" + reset + "\t" + add + "\t\t\t\t" + info + "\n\n");
+                System.out.println("\t\t" + green
+                                + "I am a BSCS-1A student of University of Northern Philippines, I am very passionate about software "
+                                + "\n\n"
+                                + "development, problem-solving, and exploring new technologies. My goal is to become a full-stack developer specializing in"
+                                + "\n\n" + "scalable web applications."
+                                + reset +
+                                "\n\n");
+                System.out.println(bblue +
+                                "---------------------------------------------------------------------------------------------------------------------------------"
+                                + reset
+                                + "\n\n");
+
+                System.out.println("\t\t\t\t\t\t" + bgreen + "<<< EDUCATIONAL BACKGROUND >>>" + reset + "\n\n\n");
+
+                // educational background
+                String eb1 = "University of Northern Philippines S.Y 2025-2026 - Bachelor of Science in Computer Science";
+                String eb2 = "Sta.Cruz Institute Inc. S.Y 2024-2025 - Humanities in Social Sciences(HUMSS) Strand";
+                String eb3 = "Sta.Cruz Institute Inc. S.Y 2023-2024";
+                String eb4 = "Santa Cruz Central School S.Y 2018-2019";
+
+                System.out.println(bgreen + "COLLEGE:" + reset + "\t\t" + eb1 + "\n");
+                System.out.println(bgreen + "SENIOR HIGH SCHOOL:" + reset + "\t" + eb2 + "\n");
+                System.out.println(bgreen + "JUNIOR HIGH SCHOOL:" + reset + "\t" + eb3 + "\n");
+                System.out.println(bgreen + "ELEMENTARY SCHOOL:" + reset + "\t" + eb4 + "\n\n\n\n");
+                System.out.println(bblue +
+                                "---------------------------------------------------------------------------------------------------------------------------------"
+                                + reset
+                                + "\n\n");
+
+                System.out.println("\t\t\t\t\t\t\t" + bgreen + "<<< SKILLS >>>" + reset + "\n");
+
+                System.out.println(bgreen + "[TECHNICAL SKILLS]" + reset + "\n");
+
+                // technical skills
+                String pl = "Proficient in Java";
+                String wd = "Knowledge of HTML";
+                String Ps = "Ability to apply algorithms in solving computational problem";
+                String tp = "Experience with Visual Studio Code, Netbeans, Intellij, and GitHub";
+
+                System.out.println(bgreen + "Programming Languages:" + reset + "\t" + pl + "\n");
+                System.out.println(bgreen + "Web Development:\t" + reset + wd + "\n");
+                System.out.println(bgreen + "Problem-Solving:\t" + reset + Ps + "\n");
+                System.out.println(bgreen + "Tools and Platforms:\t" + reset + tp + "\n\n\n");
+                System.out.println(bblue +
+                                "---------------------------------------------------------------------------------------------------------------------------------"
+                                + reset
+                                + "\n\n");
+
+                System.out.println("\t\t\t\t\t\t" + bgreen + "<<< HOBBIES and INTERESTS >>>" + reset + "\n");
+
+                System.out.println(bgreen + "[HOBBIES]" + reset + "\n");
+
+                // hobbies
+                String sports = "I engage in sports such as Volleyball (setter position), Badminton, and Chess, which strengthen my\n\n"
+                                + "\t\t\tteamwork, strategic thinking, and focus.\n\n";
+                String og = "I play Mobile Legends (especially in roaming role) and Call of Duty Mobile (in Battle Royale mode),\n\n"
+                                + "\t\t\tdeveloping teamwork, adaptability, and fast reflexes.\n\n";
+                String f = "I am commited to fitness activities such as Gym training, Weightlifting, and Jogging to build discipline,\n\n"
+                                + "\t\t\tachieve peak physique, and work toward becoming the best version of myself.";
+
+                System.out.println(bgreen + "Sports:\t\t\t" + reset + sports);
+                System.out.println(bgreen + "Online Gaming:\t\t" + reset + og);
+                System.out.println(bgreen + "Fitness & Gym:\t\t" + reset + f + "\n\n\n");
+
+                System.out.println(bgreen + "[INTERESTS]" + reset + "\n");
+
+                // interests
+                String i1 = "\t\t\t-Exploring new programming tools and technologies";
+                String i2 = "\t\t\t-Reading tech blogs and articles to keep up with innovations";
+
+                System.out.println(i1 + "\n");
+                System.out.println(i2 + "\n\n");
+
+                System.out.println(purpleb +
+                                ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+                                + reset
+                                + "\n");
+                System.out.println(bgreen + "\tPREPARED BY: James Bryan Castillo" + "\t\t\t\t\t"
+                                + "APPROVED BY: Mr. Eddiemar Sinco" + reset + "\n");
+                System.out.println(purpleb +
+                                "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+                                + reset
+                                + "\n\n");
+
+        }
+}
